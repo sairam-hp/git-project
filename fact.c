5aa1e11bee3f093740553ee6de3e0a356f5d13b4
Cloud User <ec2-user@ip-172-31-4-35.ap-south-1.compute.internal>
2026-09-24 19:14:25 +0000

    Modified .c files to remove extra lines

--- fact.c
+++ fact.c
@@ -2,17 +2,17 @@
 void factorial() {
     int n, i;
     unsigned long long fact = 1;
-    printf("\n Enter an integer: ");
+    printf("\n Sairam please Enter an integer: ");
     scanf("%d", &n);
 
     // shows error if the user enters a negative integer
     if (n < 0)
-        printf("Error! Factorial of a negative number doesn't exist.\n");
+        printf("Error! Factorial of a negative number doesn't exist.");
     else {
         for (i = 1; i <= n; ++i) {
             fact *= i;
         }
-        printf("Factorial of %d = %llu", n, fact,"\n");
+        printf("Factorial of %d = %llu", n, fact);
     }
 
    // return 0;
--- reverse.c
+++ reverse.c
@@ -3,7 +3,7 @@
 void reverse() {
   int n, reverse = 0, remainder, original;
 
-  printf("Enter an integer: \n");
+  printf("Sairam please Enter an integer: \n");
   scanf("%d", &n);
 
   original = n;  
@@ -15,14 +15,14 @@ void reverse() {
   }
 
   if (original % 10 == 0) {
-    printf("Reversed number = %d", reverse,"\n");
+    printf("Reversed number = %d", reverse);
     
     while (original % 10 == 0) {
       printf("0");
       original /= 10;
     }
   } else {
-    printf("Reversed number = %d", reverse, "\n");
+    printf("Reversed number = %d", reverse);
   }
 
 //  return 0;
