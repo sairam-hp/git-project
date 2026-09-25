c63651d1759aa93a67bdbb83e2219211238a2562
Cloud User <ec2-user@ip-172-31-4-35.ap-south-1.compute.internal>
2026-09-25 11:58:04 +0000

    fact.c file is modified

--- fact.c
+++ fact.c
@@ -2,7 +2,7 @@
 void factorial() {
     int n, i;
     unsigned long long fact = 1;
-    printf("\n Sairam please Enter an integer: ");
+    printf("\n SairamDHYAN please Enter an integer: ");
     scanf("%d", &n);
 
     // shows error if the user enters a negative integer
